49e0b4e86b03c36b537ea26f2e9fb1fa17266ac1
LeoNick85 <danielenicolini.dev@gmail.com>
2024-09-01 14:55:11 +0200

    avvio progetto

---
+++ sample-spring-backend/src/main/java/com/leonick/sample_spring_backend/SampleSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leonick.sample_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBackendApplication.class, args);
+	}
+
+}
---
+++ sample-spring-backend/src/test/java/com/leonick/sample_spring_backend/SampleSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonick.sample_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
